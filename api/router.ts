33cbcfd2adddadcc52b3b4beaa9ce8b7d655a65f
Mihai-Maxim <135857206+mihaimaximthinslices@users.noreply.github.com>
2023-10-10 18:37:59 +0300

    update scripts

--- api/router.ts
+++ api/router.ts
@@ -6,13 +6,13 @@ const router = Router()
 router.post('/api/register', withErrorHandling(registerUserController, sharedErrorHandler))
 
 router.post('/api/login', withErrorHandling(loginUserController, sharedErrorHandler))
-router.get('/status', (req: Request, res: Response) => {
+router.get('/api/status', (req: Request, res: Response) => {
   return res.status(200).json({
     message: 'logged in',
   })
 })
 
-router.get('/test', (req: Request, res: Response) => {
+router.get('/api/test', (req: Request, res: Response) => {
   res.json('test')
 })
 export default router
